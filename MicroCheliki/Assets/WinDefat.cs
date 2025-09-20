06b59250d8f7f52cd42ca580477a047b257cd8c4
Bananaska <romanzlobin10@mail.ru>
2025-09-20 17:03:09 +0300

    roma1

---
+++ MicroCheliki/Assets/WinDefat.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WinDefat : MonoBehaviour
+{
+    public bool win = false;
+    public bool defat = false;
+    public bool touchBoss = false;
+    int cloneScore = 100;
+
+    public Animator animator;
+
+    
+    void Update()
+    {
+        if (cloneScore >= 100 && touchBoss == true)
+        {
+            win = true;
+            animator.SetBool("win", true);
+        }
+        if (cloneScore <= 100 && touchBoss == true)
+        {
+            defat = true;
+            animator.SetBool("def", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            touchBoss = true;
+        }
+    }
+}
